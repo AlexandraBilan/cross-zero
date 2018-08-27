df696e7ff6e2e2183b9c4482871b5569b6adeb94
Alexandra Bilan <k-a-s-n-d-r-a@mail.ru>
2018-08-27 23:54:59 +0300

    Updated winner search function.

--- cross-zero.js
+++ cross-zero.js
@@ -1,124 +1,80 @@
 var table = document.getElementById("table");
 var flag = true;
-var rows = table.rows;
-function start(){
-table.onclick = function(event)
-{
-    var target = event.target;
-    if (target.tagName != 'TD') return; //проверка, тыкнули ли мы на td
-    if(target.innerHTML === "")
-    {
-            if(flag)
-        {
-            target.style = "color: #3b00ff";
-            target.innerHTML = "x";
-            flag = false;
-            check();
-            return;
-        }
 
-    if(!flag)
-        {
-            target.style = "color: red";
-            target.innerHTML = "o";
-            flag = true;
-            check();
-            return;
-        }
+function start() {
+    table.onclick = function (event) {
+        var target = event.target;
+        if (target.tagName != 'TD') return; //проверка, тыкнули ли мы на td
+        if (target.innerHTML === "") {
+            if (flag) {
+                target.style = "color: #3b00ff";
+                target.innerHTML = "x";
+                flag = false;
+                check();
+                return;
+            }
+
+            if (!flag) {
+                target.style = "color: red";
+                target.innerHTML = "o";
+                flag = true;
+                check();
+                return;
+            }
+        } else return;
     }
-    else return;
 }
-}
+
 var clear_td = document.getElementsByTagName("td");
-function clear()
-{
-   
-    for(var i=0; i<clear_td.length; i++)
-    {
-      clear_td[i].innerHTML="";
-     }
-    document.getElementById('result').innerHTML = "";
-    document.getElementById('win1').style.display = "none";
-    document.getElementById('win2').style.display = "none";
+
+function clear() {
+
+    for (var i = 0; i < clear_td.length; i++) {
+        clear_td[i].innerHTML = "";
+    }
+    for (var j = 0; j <= 7; j++) {
+        document.getElementById('win' + j).style.display = "none";
+    }
+    document.getElementById('rezult').innerHTML = "";
     start();
     flag = true;
 }
 
+var win_cod = ['012', '345', '678', '036', '148', '258', '048', '642'];
 
-function check()
-{
+function check() {
     let winner = "Победили ";
-       if(rows[0].cells[0].textContent === rows[0].cells[1].textContent && rows[0].cells[1].textContent === rows[0].cells[2].textContent && rows[0].cells[0].textContent != "")
-                {
-                    document.getElementById('win2').style = "display: block; left: 125px; top: 95px;";
-                    document.getElementById('result').innerHTML = winner + " " +rows[0].cells[0].textContent;
-                    table.onclick=function (){return false};
-                    return;
+    var arr = new Array();
+
+    for (var r = 0, n = table.rows.length; r < n; r++) {
+
+        for (var c = 0, m = table.rows[r].cells.length; c < m; c++) {
+            arr.push(table.rows[r].cells[c].innerText);
+        }
+    }
+    for (var i = 0; i < win_cod.length; i++) {
+        if (arr[win_cod[i].charAt(0)] === arr[win_cod[i].charAt(1)] && arr[win_cod[i].charAt(1)] === arr[win_cod[i].charAt(2)] && arr[win_cod[i].charAt(0)] != '') {
+            document.getElementById('rezult').innerText = winner + " " + arr[win_cod[i].charAt(0)];
+            document.getElementById('win' + i).style.display = "block";
+            table.onclick = function () {
+                return false;
+            };
+            break;
+        } else {
+            var counter = 0;
+            for (var j = 0; j < clear_td.length; j++) {
+                if (clear_td[j].textContent != "") {
+                    counter++;
+                    if (counter === 9) {
+                        document.getElementById('rezult').style = "margin-left: 190px;";
+                        document.getElementById('rezult').innerHTML = "Ничья :)";
+                    }
                 }
-       else if(rows[1].cells[0].textContent === rows[1].cells[1].textContent && rows[1].cells[1].textContent === rows[1].cells[2].textContent && rows[1].cells[0].textContent != "")
-                {
-                   document.getElementById('win2').style = "display: block; left: 125px; top: 155px;";
-                    document.getElementById('result').innerHTML = winner + " " +rows[1].cells[0].textContent;
-                    table.onclick=function (){return false};
-                    return;
-                }    
-      else if(rows[2].cells[0].textContent === rows[2].cells[1].textContent && rows[2].cells[1].textContent === rows[2].cells[2].textContent && rows[2].cells[0].textContent != "")
-                {
-                   document.getElementById('win2').style = "display: block; left: 125px; top: 215px;";
-                   document.getElementById('result').innerHTML = winner + " " +rows[2].cells[0].textContent;
-                    table.onclick=function (){return false};
-                   return;
-                } 
-     else if(rows[0].cells[0].textContent === rows[1].cells[0].textContent && rows[1].cells[0].textContent === rows[2].cells[0].textContent && rows[0].cells[0].textContent != "")
-                {
-                   document.getElementById('win2').style = "display: block; transform: rotate(90deg); top: 140px; left: 76px;"; 
-                   document.getElementById('result').innerHTML = winner + " " +rows[0].cells[0].textContent;
-                    table.onclick=function (){return false};
-                    return;
-                } 
-      else if(rows[0].cells[1].textContent === rows[1].cells[1].textContent && rows[1].cells[1].textContent === rows[2].cells[1].textContent && rows[0].cells[1].textContent != "")
-                {
-                     document.getElementById('win2').style = "display: block; transform: rotate(90deg); top: 140px; left: 130px;"; 
-                    document.getElementById('result').innerHTML = winner + " " +rows[0].cells[1].textContent;
-                    table.onclick=function (){return false};
-                    return;
-                } 
-      else if(rows[0].cells[2].textContent === rows[1].cells[2].textContent && rows[1].cells[2].textContent === rows[2].cells[2].textContent && rows[0].cells[2].textContent != "")
-                {
-                    document.getElementById('win2').style = "display: block; transform: rotate(90deg); top: 140px; left: 190px;"; 
-                  document.getElementById('result').innerHTML = winner + " " +rows[0].cells[2].textContent;
-                    table.onclick=function (){return false};
-                    return;
-                } 
-      else if(rows[0].cells[0].textContent === rows[1].cells[1].textContent && rows[1].cells[1].textContent === rows[2].cells[2].textContent && rows[0].cells[0].textContent != "")
-                {
-                     document.getElementById('win1').style = "display: block;  transform: rotate(45deg); left: 105px; top: 155px" 
-                    document.getElementById('result').innerHTML = winner + " " +rows[0].cells[0].textContent;
-                    table.onclick=function (){return false};
-                    return;
-                } 
-      else if(rows[0].cells[2].textContent === rows[1].cells[1].textContent && rows[1].cells[1].textContent === rows[2].cells[0].textContent && rows[0].cells[2].textContent != "")
-                {
-                    document.getElementById('win1').style = "display: block;  transform: rotate(132deg); left: 95px; top: 155px;"; 
-                    document.getElementById('result').innerHTML = winner + " " +rows[0].cells[2].textContent;
-                    table.onclick=function (){return false};
-                    return;
-                } 
-    else
-        {
-    var counter = 0;
-    for (var i = 0; i < clear_td.length; i++) {
-        if (clear_td[i].textContent != "") 
-        {
-            counter++;
-            if (counter === 9) {
-                document.getElementById('result').style = " margin-left: 190px;";
-                document.getElementById('result').innerHTML = "Ничья :)";
             }
         }
     }
-    }
-
 }
+
+
 window.addEventListener('DOMContentLoaded', start);
 btn.addEventListener('click', clear);
